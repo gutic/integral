f6952838e4463956fdcfa68a102d53e0c595c2e3
Agustin Terlaak <agustinterlaak@gmail.com>
2017-09-05 05:14:30 -0300

    Integra funciones del tipo: 3x^3-x^2+x-1

--- integrar/integrar/Properties/Resources.Designer.cs
+++ integrar/integrar/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace derive.Properties {
+namespace integrales.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace derive.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("derive.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("integrales.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
