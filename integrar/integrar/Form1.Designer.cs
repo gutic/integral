4346244445befe3b29bdbfbfda253b4afd8c5d98
Agustin Terlaak <agustinterlaak@gmail.com>
2017-09-05 05:10:18 -0300

    Integral funcion simple estilo: 3x^3-x^2+x-1

---
+++ integrar/integrar/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace derive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Ingrese f(x) aquí";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(224, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Integrar!";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(433, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 93);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "dx =";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(151, 53);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(30, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "F(x)=";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(112, 68);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(21, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "A";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(112, 16);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(21, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "B";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(79, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(36, 42);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(345, 53);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(18, 20);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.Text = "dx";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(224, 138);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 13;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(190, 138);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(28, 20);
+            this.textBox11.TabIndex = 15;
+            this.textBox11.Text = "=";
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(594, 192);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+    }
+}
+
---
+++ integrar/integrar/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace derive
+{
+
+    public partial class Form1 : Form
+    {
+        public double A; 
+        public double B; 
+        public double funcionA;
+        public double funcionB;
+        public double resultado;
+        public string result;
+        public string funcion;
+        public int cuanto;
+        public int contador = 0;
+        public int TotalMono = 0;
+        public char[] grado = { 'x', '^' };
+        public string submono;
+        public string [] parte;
+        public int TotalParte;
+        public int MaxGrado;
+        public int multi;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.BackColor = Color.Black;
+        }
+        System.Drawing.Pen Lapiz = new System.Drawing.Pen(System.Drawing.Color.Blue);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics formGraphics = panel1.CreateGraphics();
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox5.Text) || string.IsNullOrEmpty(textBox6.Text) ||  string.Format(textBox1.Text) == "Ingrese f(x) aquí") {
+                MessageBox.Show("Ingrese una funcion! \n");
+            }
+            else
+            {
+                funcion = (textBox1.Text);
+                if (textBox5.Text == "A" || textBox6.Text == "B")
+                {
+                    MessageBox.Show("Ingrese valores para A y B!");
+                }
+                else { 
+                    A = Convert.ToDouble(textBox5.Text);
+                    B = Convert.ToDouble(textBox6.Text);
+                    if (A == B)
+                    {
+                        textBox2.Text = Convert.ToString("0");
+                    }
+                    else
+                    {
+                        textBox2.Text = Convert.ToString(integrar(funcion));
+                        resultado = funcionA - funcionB;
+                        result =  Convert.ToString(resultado);
+                        textBox10.Text=Convert.ToString(result);
+                        funcionA = 0;
+                        funcionB = 0;
+                    }
+                }
+
+            }
+
+        }
+
+        public string integrar(string funcion)
+        {
+            string salida = "";
+            string[] terminos;
+            string dx;
+            bool EsPositivo = true;
+            double potencia = 0, coeficiente = 0;
+            funcion = formato(funcion); //separo -  +
+            terminos = funcion.Split();
+            foreach (string termino in terminos)
+            {
+                    separar(termino,ref coeficiente,ref potencia,ref EsPositivo);
+                    dx = integrarTermino(coeficiente, potencia, EsPositivo);
+                    salida += dx;
+            }
+            if  (!(salida.Trim().Length > 0))
+            {
+                return "x";
+            }
+            if (salida[1] == '+')
+            {
+                return salida.Substring(3);
+            }
+            if (salida[1] == '-')
+            {
+                return Convert.ToString(salida[1]) + salida.Substring(3);
+            }
+            if (salida[0] == ' ')
+            {
+                return salida.Substring(1);
+            }
+            return salida;
+        }
+        public string formato(string funcion)//separo estilo  1 +2 -6
+        {
+            string salir = "";
+            int j = 0;
+            for (int i = 0; i < funcion.Length - 1; i++)
+            {
+                if ((funcion[i] == '-' || funcion[i] == '+') && i > 0)
+                {
+                    if (funcion[i - 1] != '^')
+                    {
+                        salir += funcion.Substring(j, i - j) + " "; //concateno y agrego espacio antes del + o -
+                        j = i;
+                    }
+                }
+            }
+            salir += funcion.Substring(j, funcion.Length - j);
+            return salir;
+        }
+        private void separar(string termino,ref double coeficiente,ref double potencia,ref bool EsPositivo)
+        {
+            int fincoeficiente = 0;
+            int inicio;
+            EsPositivo = true;
+            if (termino[0] == '-')
+            {
+                EsPositivo = false;
+                inicio = 1;
+            } 
+            else if (termino[0] == '+')
+            {
+                EsPositivo = true;
+                inicio = 1;
+            }
+            else
+            {
+                inicio = 0;
+            }
+            if(termino == Convert.ToString('x'))
+            {
+                coeficiente = 1; //cuento numero de coeficientes
+            }
+            else
+            {
+                for(int i=0;i< termino.Length -1; i++)
+                {
+                    if (!(char.IsNumber(termino[i])))
+                    {
+                        fincoeficiente = i - 1; //veo donde termina el coeficiente
+                        break;
+                    }
+                }
+                string c;
+                if (fincoeficiente <= 0)
+                {
+                    c = termino.Substring(inicio, 1);
+                    
+                }
+                else if(inicio == 0)
+                {
+                    c = termino.Substring(inicio, fincoeficiente + 1);
+                    
+                }
+                else
+                {
+                    c = termino.Substring(inicio, fincoeficiente);
+                    
+                }
+                if (c == Convert.ToString('x') || c.Length == 0)
+                {
+                    coeficiente = 1;
+                }
+                else
+                {
+                    if (Convert.ToString(c) != string.Empty)
+                    {
+                        //MessageBox.Show(c);
+                        double valor;
+                        double.TryParse(c, out valor);
+                        //MessageBox.Show(Convert.ToString(valor));
+                        coeficiente = valor;
+                    }
+                }
+            }
+            //saco la potencia
+            if (termino.IndexOf("^") >= 0 && termino.IndexOf("(") < 0)
+            {
+                potencia = Convert.ToInt32(termino.Substring(termino.IndexOf("^") + 1));
+            }
+            else if(termino.IndexOf("x") >=0 && termino.IndexOf("^") < 0)
+            {
+                potencia = 1;
+            }
+            //fraccion no funciona, retorno//
+            else if (termino.IndexOf(")") >= 0) 
+            {
+                string fraccion;
+                inicio = termino.IndexOf("(") + 1;
+                fraccion = termino.Substring(inicio, (termino.Length - inicio) - 1);
+                potencia = fraccionADecimal(fraccion);
+            }
+            else
+            {
+                potencia = 0;
+            }
+        }
+        private Single fraccionADecimal(string fraccion)
+        {
+            int primer, segundo;
+            primer = Convert.ToInt32(fraccion.Substring(0, fraccion.IndexOf("/")));
+            segundo = Convert.ToInt32(fraccion.Substring(fraccion.IndexOf("/") + 1));
+            //MessageBox.Show(Convert.ToString(primer / segundo));
+            return primer / segundo;
+        }
+        private string integrarTermino(double coeficiente, double potencia, bool EsPositivo)
+        {
+            string salida = "";
+            double termino;
+            if (!(EsPositivo))
+            {
+                if (potencia < 0)
+                {
+                    salida += " + ";
+                }
+                else
+                {
+                    salida += " - ";
+                }
+            }
+            else if (potencia >= 0)
+            {
+                salida += " + ";
+            }
+            else
+            {
+                salida += " - ";
+            }
+            termino = Math.Abs(coeficiente);
+            Convert.ToDouble(A);
+            Convert.ToDouble(potencia);
+            funcionA += (coeficiente * (Math.Pow(Convert.ToDouble(A), Convert.ToDouble(potencia + 1)))) / (potencia + 1);
+            funcionB += (coeficiente * (Math.Pow(Convert.ToDouble(B), Convert.ToDouble(potencia+1)))) / (potencia+1);
+            
+            switch (Convert.ToInt32(potencia))
+            {
+                case 1:
+                    return "(" + salida + termino + "x^" + (potencia + 1) + ")" + "/" + "(" + (potencia + 1) + ")";
+                    
+                case 2:
+                    return "(" + salida + termino + "x^" + (potencia + 1) + ")" + "/" + "(" + (potencia + 1) + ")";
+                case 0:
+                    return salida + "x";
+
+                default:
+                    if (termino != 1)
+                    {
+                        return "(" + salida + termino + "x^" + (potencia + 1) + ")" + "/" + "(" + (potencia + 1) + ")"; ;
+                    }
+                    else
+                    {
+                        return salida + "x^" + (potencia - 1);
+                    }
+            }
+        }
+
+
+
+
+
+
+
+
+
+        //BARRA SEPARADORA DE BAJO PRESUPUESTO BARRA SEPARADORA DE BAJO PRESUPUESTO BARRA SEPARADORA DE BAJO PRESUPUESTO BARRA SEPARADORA DE BAJO PRESUPUESTO 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)      //dibujo el grafico + para la funcion
+        {
+            //int xcentro = panel1.Width / 2;
+            //int ycentro = panel1.Height / 2;
+            //e.Graphics.TranslateTransform(xcentro, ycentro);
+            //e.Graphics.ScaleTransform(1, - 1);
+            //e.Graphics.DrawLine(Lapiz, xcentro * -1, 0, xcentro * 2, 0); //ejex
+            //e.Graphics.DrawLine(Lapiz, 0, ycentro, 0, ycentro * -1); // ejey
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
